a9fa1c3afe99bcfd93da766d1b7b9881424fa310
vsc34871 <vsc34871@tier2-p-login-1.genius.hpc.kuleuven.be>
2024-12-02 16:40:32 +0100

    Update from HPC

--- _targets.R
+++ _targets.R
@@ -20,10 +20,10 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "auto-sdms",
-      per_cpu_mem = "4000mb",
+      per_cpu_mem = "8000mb",
       n_tasks = 1,
       per_task_cpus = 36,
-      walltime = "15:00:00"
+      walltime = "20:00:00"
     ))
   )
 )
@@ -36,17 +36,17 @@ tar_source()
 tar_plan(
   # Load the required paths
   input_folders = list(
-    ForestClim_12 = "E:/Output/SDM_test/belgium/tiles/ForestClim_12/",
-    ForestClim_15 = "E:/Output/SDM_test/belgium/tiles/ForestClim_15/",
-    ForestClim_05 = "E:/Output/SDM_test/belgium/tiles/ForestClim_05/",
-    ForestClim_06 = "E:/Output/SDM_test/belgium/tiles/ForestClim_06/",
-    cec = "E:/Output/SDM_test/belgium/tiles/cec/",
-    clay = "E:/Output/SDM_test/belgium/tiles/clay/"
+    ForestClim_12 = "/lustre1/scratch/348/vsc34871/output/tiles/ForestClim_12/",
+    ForestClim_15 = "/lustre1/scratch/348/vsc34871/output/tiles/ForestClim_15/",
+    ForestClim_05 = "/lustre1/scratch/348/vsc34871/output/tiles/ForestClim_05/",
+    ForestClim_06 = "/lustre1/scratch/348/vsc34871/output/tiles/ForestClim_06/",
+    cec = "/lustre1/scratch/348/vsc34871/output/tiles/cec/",
+    clay = "/lustre1/scratch/348/vsc34871/output/tiles/clay/"
   ),
   mdl_paths = list.files(
-    "E:/SDMs/Stef_SDMs/Models/",
+    "/lustre1/scratch/348/vsc34871/input/Models/",
     full.names = TRUE
   ),
   # Make future species distributions
-  tar_target(predict_futSDM, predict_futSDM(input_folders, mdl_paths))
+  tar_target(futureSDMs, predict_futSDM(input_folders, mdl_paths))
 )
