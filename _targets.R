04b4b4de98ce337d43c8c424616ebd8ed46add88
XiaquZ <xiaqu.zhou@gmail.com>
2025-09-22 09:37:03 +0200

    Update models and data paths.

--- _targets.R
+++ _targets.R
@@ -35,22 +35,22 @@ tar_source()
 tar_plan(
   # Load the required paths
     input_folders = list(
-    cec = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/cec/",
-    CHELSA_bio12_EU_2000.2019 = "",
-    CHELSA_bio15_EU_2000.2019 = "",
-    clay = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/clay/",
-    Elevation = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/Elevation/",
-    Micro_BIO5_EU_CHELSAbased_2000.2020 = "",
-    Micro_BIO6_EU_CHELSAbased_2000.2020 = "",
-    Slope = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/Slope/",
-    TWI = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/TWI/",
-    phh2o_0_30_WeightedMean = "/lustre1/scratch/348/vsc34871/SDM_current/pred_tiles/phh2o_0_30_WeightedMean/"
+    cec = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/cec/",
+    CHELSA_bio12_EU_2000.2019 = "/lustre1/scratch/348/vsc34871/SDM_fut/pred_tiles/CHELSA_bio12_EU_2000.2019/",
+    CHELSA_bio15_EU_2000.2019 = "/lustre1/scratch/348/vsc34871/SDM_fut/pred_tiles/CHELSA_bio15_EU_2000.2019/",
+    clay = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/clay/",
+    Elevation = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/Elevation/",
+    Micro_BIO5_EU_CHELSAbased_2000.2020 = "/lustre1/scratch/348/vsc34871/SDM_fut/pred_tiles/Micro_BIO5_EU_CHELSAbased_2000.2020/",
+    Micro_BIO6_EU_CHELSAbased_2000.2020 = "/lustre1/scratch/348/vsc34871/SDM_fut/pred_tiles/Micro_BIO6_EU_CHELSAbased_2000.2020/",
+    Slope = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/Slope/",
+    TWI = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/TWI/",
+    phh2o_0_30_WeightedMean = "/lustre1/scratch/348/vsc34871/SDM_current/pred_bigtiles/phh2o_0_30_WeightedMean/"
   ),
   # Note that although the input folders named bio5, bio6, bio12, and bio15 are represented 2000-2000,
   # they actually contain future climate data for 2071-2100 under the SSP3-7.0 scenario.
   tar_target(mdl_paths,
     list.files(
-    "/lustre1/scratch/348/vsc34871/SDM_fut/Models/",
+    "/lustre1/scratch/348/vsc34871/SDM_current/Models/",
     full.names = TRUE
   )),
   # Make future species distributions
