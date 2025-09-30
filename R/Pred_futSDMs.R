c149e37d7d5c605d7b4ec4816641bc61a04cb2df
XiaquZ <xiaqu.zhou@gmail.com>
2025-09-30 11:52:06 +0200

    Update to models03.

--- R/Pred_futSDMs.R
+++ R/Pred_futSDMs.R
@@ -1,7 +1,7 @@
 predict_futSDM <- function(input_folders, mdl_paths) {
 
   # Iterate through tiles (assumes tiles are numbered from 1 to 9)
-  for (i in 5:20) {
+  for (i in 1:8) {
     # Initialize an empty list to store predictors for this tile
     # Define predictor keywords
     predictors <- c(
